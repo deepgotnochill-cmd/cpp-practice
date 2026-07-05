4a385e1717042c551bee1218ef72263cd7dfcd0c
deepgotnochill-cmd <dattachaudhurydeep17@gmail.com>
2026-07-05 18:58:37 +0530

    loops cpp

--- HW/class work/compositeorprime.cpp
+++ HW/class work/compositeorprime.cpp
@@ -5,13 +5,17 @@ int main(){
         int n;
     cout<<"Enter a number please : ";
     cin>>n;
-    int factors = 0;
-    for(int i = 1; i <=sqrt(n) ; i ++){
+    if(n<=1){
+        cout << "neither prime or composite number";
+        return 0;
+    }
+    bool flag = false ; // false means prime
+    for(int i = 2; i <= sqrt(n) ; i ++){
         if(n%i == 0){ // factors mil gaya
-            factors++;
+            flag = true;
+            break;
     }
 }
-  if(factors ==1) cout<<"neither prime nor composite ";
-  else if( factors >= 3) cout<<"composite number";
-  else cout<<"prime number";
+  if(flag == true ) cout << "composite number ";
+  else cout << "prime number";
 }
---
+++ HW/class work/infiniteloop.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    while(1<2){
+        cout<<"i can do it"<<endl;
+    }
+}
