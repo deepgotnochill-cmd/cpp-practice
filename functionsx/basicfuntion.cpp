ba18d95ebc6e6a416418fcb8d1497da8881eed9b
deepgotnochill-cmd <dattachaudhurydeep17@gmail.com>
2026-07-19 17:43:24 +0530

    today's cpp practice (i started with functions)

---
+++ functionsx/basicfuntion.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+void tisha(){
+   cout<<"Tisha is an obidient girl"<<endl;
+}
+void deep(){
+   cout<<"Deep is an obidient boy"<<endl;
+   tisha();
+}
+int main(){
+    tisha();
+    deep();
+   cout<<"Hi Tisha"<<endl;
+   deep();//fn calling
+   tisha();
+}
