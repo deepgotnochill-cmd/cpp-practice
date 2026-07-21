13afa0dfe1e5fe1f257482f323e43a723779e744
deepgotnochill-cmd <dattachaudhurydeep17@gmail.com>
2026-07-21 13:54:45 +0530

    today's cpp practice

--- functionsx/startriangle.cpp
+++ functionsx/startriangle.cpp
@@ -1,31 +1,27 @@
 #include <iostream>
 using namespace std;
+void starTriangle(int n){
+    int n;
+    cout<<"enter the size of star triangle you want for n : ";
+    cin>>n;
+    for (int i=1; i <= n ; i++){
+        for (int j = 1 ; j <= i ; j++){
+            cout<<"*"<<" ";
+        }
+        cout<<endl; 
+    }
+}
 int main(){
     int a;
     cout<<"enter the size of star triangle you want for a : ";
     cin>>a;
-    for (int i=1; i <= a ; i++){
-        for (int j = 1 ; j <= i ; j++){
-            cout<<"*"<<" ";
-        }
-        cout<<endl;
-    }
+   starTriangle(a);
       int b;
     cout<<"enter the size of star triangle you want for b : ";
     cin>>b;
-    for (int i=1; i <= b ; i++){
-        for (int j = 1 ; j <= i ; j++){
-            cout<<"*"<<" ";
-        }
-        cout<<endl;
-} 
+    starTriangle(b);
       int c;
     cout<<"enter the size of star triangle you want for c : ";
     cin>>c;
-    for (int i=1; i <= c ; i++){
-        for (int j = 1 ; j <= i ; j++){
-            cout<<"*"<<" ";
-        }
-        cout<<endl;
-}
+    starTriangle(c);
 }
