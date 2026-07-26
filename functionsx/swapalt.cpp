8bd9142f94af8b04cf977886806781dddbb5504c
deepgotnochill-cmd <dattachaudhurydeep17@gmail.com>
2026-07-26 13:39:33 +0530

    trying new format to swap numbers

--- functionsx/swapalt.cpp
+++ functionsx/swapalt.cpp
@@ -2,8 +2,10 @@
 using namespace std;
 int main(){
     int a = 8 , b = 7;
-    a=a+b;
-    b=a-b;
-    a=a-b;
-    cout<<a<<" "<<b;
+    cout<<a<<" "<<b<<endl;
+    a=(a+b)-(b=a);
+    // a=a+b;
+    // b=a-b;
+    // a=a-b;
+    cout<<a<<" "<<b<<endl;
 }
