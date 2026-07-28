8b8f1ebea15927901db2c089463770987b048074
deepgotnochill-cmd <dattachaudhurydeep17@gmail.com>
2026-07-28 09:36:34 +0530

    Todays pointers in cpp practice

--- Pointers/SwapUsingPointers.cpp
+++ Pointers/SwapUsingPointers.cpp
@@ -1,9 +1,9 @@
 #include<iostream>
 using namespace std;
-void swap(int* a , int* b){
-    int temp =*a;
-    *a=*b;
-    *b = temp;
+void swap(int* p1 , int* p2){
+    int temp =*p1;
+    *p1=*p2;
+    *p2 = temp;
 }
 int main(){
 int a = 5, b = 2;
