510ae873e623fa203ddd330cd1a7013856769639
deepgotnochill-cmd <dattachaudhurydeep17@gmail.com>
2026-08-11 01:05:39 +0530

    todays cpp practice

---
+++ 2D_Array/tempCodeRunnerFile.cpp
@@ -0,0 +1,18 @@
+using namespace std;
+
+int main(){
+    //int arr[3][4];
+    int arr [][4]={{5,8,1,2},{9,9,4,4},{7,0,3,5}};
+    int maxRow = -1 ,  maxSum = INT_MIN;
+    for(int i = 0 ; i < 3 ; i++){
+        int sum = 0;
+        for(inr j = 0 ; j<4 ; j++){
+            sum += arr[i][j];
+        }
+        if(sum > maxSum){
+            maxSum = sum ;
+            maxRow = i ;
+        }
+    }
+    cout<<maxRow<<" "<<maxSum;
+}    
