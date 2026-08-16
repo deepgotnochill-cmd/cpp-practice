97148f814585fbd9953c430a72a5ebb77a2bc86d
deepgotnochill-cmd <dattachaudhurydeep17@gmail.com>
2026-08-17 01:04:08 +0530

    todays cpp practice

---
+++ Sorting_Algorithm/SelectionSort.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespace std; 
+void print(vector<int> &arr){
+    for(int ele : arr){
+        cout<<ele<<" ";
+    }
+    cout<<endl;
+}
+int main(){
+    vector<int> arr = {5,4,3,6,2,1};
+    int n = arr.size();
+    print(arr);
+    int mn = arr[0],mnIdx = 0;
+    for(int i = 0 ; i<n ; i++){
+        if (arr[i]<mn){
+            mn=arr[i];
+            mnIdx = i;
+        }
+    }
+    swap(arr[0],arr[mnIdx]);
+    print(arr);
+}
