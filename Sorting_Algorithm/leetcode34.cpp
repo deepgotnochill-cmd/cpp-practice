4e9835602b55f1939b5192e0e9525739308b213b
deepgotnochill-cmd <dattachaudhurydeep17@gmail.com>
2026-08-22 23:16:24 +0530

    todays cpp practice

---
+++ Sorting_Algorithm/leetcode34.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+// First Occurance
+    vector<int> searchRange(vector<int>& arr, int target) {
+          int n = arr.size();
+          vector<int> ans(2,-1);
+        int lo = 0, hi = n-1;
+        while(lo<=hi){
+            int mid = (lo + hi)/2;
+            if(arr[mid] > target) hi = mid - 1;
+            else if(arr[mid] < target) lo = mid + 1;
+            else{
+                ans[0] = mid;
+                hi = mid - 1;
+            }
+        }
+             lo = 0;
+             hi = n-1;
+        while(lo<=hi){
+            int mid = (lo + hi)/2;
+            if(arr[mid] > target) hi = mid - 1;
+            else if(arr[mid] < target) lo = mid + 1;
+            else{
+                ans[1] = mid;
+                lo = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
