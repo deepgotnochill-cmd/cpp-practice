ba1a8a97ca89174bebd3d8c4a68aa6a55727f0cf
deepgotnochill-cmd <dattachaudhurydeep17@gmail.com>
2026-08-28 23:24:10 +0530

    todays cpp practice

--- Strings/vowels.cpp
+++ Strings/vowels.cpp
@@ -1,13 +1,22 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
+using namespace std;
+
 int main(){
     string s = "Mohit Kumar Anand";
+    cout << s << endl;
     int n = s.length();
     int count = 0;
+
     for(int i = 0 ; i < n ; i++){
         char ch = s[i];
-        if(ch>=65 and ch<=90)ch+=32;
-        if(ch =="a" or ch =="e" oe ch =="i" or ch=="o" or ch=="u")
+
+        if(ch >= 65 and ch <= 90)
+            ch += 32;
+
+        if(ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u')
+            count++;
     }
-cout<<count<<endl;
+
+    cout << count << endl;
 }
