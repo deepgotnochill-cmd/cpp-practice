8f4c4b18fc97157a1473d8949cb361c4eaeee187
deepgotnochill-cmd <dattachaudhurydeep17@gmail.com>
2026-08-29 22:15:35 +0530

    todays cpp practice

--- Strings/basic.cpp
+++ Strings/basic.cpp
@@ -2,9 +2,12 @@
 #include<string>
 using namespace std;
 int main(){
-    string x = "Raghav";
+    string x = "Deep";
     cout<<x<<endl;
     string s;
+    string z;
+    cin>>z;
+    cout<<z<<endl;
     cin>>s;
     cout<<s;
     string y;
