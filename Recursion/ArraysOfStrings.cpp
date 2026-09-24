474d764896ec9e0b0d5cf24acab96039923bf540
deepgotnochill-cmd <dattachaudhurydeep17@gmail.com>
2026-09-24 20:16:25 +0530

    Todays cpp practice

---
+++ Recursion/ArraysOfStrings.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main() {
+    vector<string> arr = {"Raghgav", "Yash", " Manish", "Arman", "Shubham"};
+
+    cout << arr[2][3] << endl;
+
+    return 0;
+}
